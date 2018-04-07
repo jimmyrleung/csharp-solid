b507ef9d3c0bd85c710bafbbb6f6bdedd2591509
jimmy <jimmy@DESKTOP-440J00T>
2018-04-07 17:45:53 -0300

    Add project files.

---
+++ csharp-solid/CalculadoraDeSalario.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_solid
+{
+    public class CalculadoraDeSalario
+    {
+        public double calcula(Funcionario funcionario)
+        {
+            if (funcionario.Cargo is Desenvolvedor)
+            {
+                return dezOuVintePorcento(funcionario);
+            }
+
+            if (funcionario.Cargo is Dba || funcionario.Cargo is Tester)
+            {
+                return quinzeOuVinteCincoPorcento(funcionario);
+            }
+
+            throw new Exception("funcionario invalido");
+        }
+
+        private double dezOuVintePorcento(Funcionario funcionario)
+        {
+            if (funcionario.SalarioBase > 3000.0)
+            {
+                return funcionario.SalarioBase * 0.8;
+            }
+            else
+            {
+                return funcionario.SalarioBase * 0.9;
+            }
+        }
+
+        private double quinzeOuVinteCincoPorcento(Funcionario funcionario)
+        {
+            if (funcionario.SalarioBase > 2000.0)
+            {
+                return funcionario.SalarioBase * 0.75;
+            }
+            else
+            {
+                return funcionario.SalarioBase * 0.85;
+            }
+        }
+    }
+}
---
+++ csharp-solid/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_solid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculadoraDeSalario calculadora = new CalculadoraDeSalario();
+            Funcionario dev = new Funcionario(new Desenvolvedor(), 2000);
+            Console.WriteLine(String.Format("Salário Bruto: {0}\nSalário Líquido: {1}", dev.SalarioBase, calculadora.calcula(dev)));
+            System.Threading.Thread.Sleep(20000);
+        }
+    }
+}
